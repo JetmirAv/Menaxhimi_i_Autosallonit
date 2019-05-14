2fb45fcc30890894fd58cafe182468a74560f7de
jetmir.avdullahu@studentet.uni-pr.edu <jetmir.avdullahu@studentet.uni-pr.edu>
2019-05-15 00:55:02 +0200

    Header View Start

--- Autosalloni/src/view/Header.java
+++ Autosalloni/src/view/Header.java
@@ -1,13 +1,57 @@
 package view;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class Header extends Application{
+import java.io.FileInputStream;
+import java.io.IOException;
 
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+
+
+public class Header{
+
+	public static HBox display(String fullName, String role, String current) throws IOException {
+		
+		
+		
+		
+		
+		
+		HBox header = new HBox(20);
+		header.setAlignment(Pos.BASELINE_RIGHT);
+		header.setStyle("-fx-border-color: black; -fx-height: 50px; -fx-padding: 10px 15px");
+		
+		FileInputStream logOutPath = new FileInputStream(current + "/src/img/power.png"); 
+		Image logOut = new Image(logOutPath);
+		ImageView logOutView = new ImageView(logOut);
+		logOutView.setFitHeight(30);
+		logOutView.setFitWidth(30);
+
+		Label labelLogOut = new Label();
+		labelLogOut.setGraphic(logOutView);
+		
+		VBox userBox = new VBox(10);
+		
+		Label userFullName = new Label(fullName);
+		Label userRole = new Label(fullName);
+
+		userFullName.setStyle("-fx-font-family: \"Sans-serif\"; -fx-font-size: 20px");
+		userRole.setStyle("-fx-font-family: \"knk\"; -fx-font-size: 20px");
+		userBox.getChildren().addAll(userFullName, userRole);
+		
+		
+		
+		
+		
+		header.getChildren().addAll(userBox, labelLogOut);
+		
+		return header;
 		
 	}
 }
---
+++ Autosalloni/src/view/Main.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		String current = new java.io.File( "." ).getCanonicalPath();
+		Font.class.getResource(current + "/src/fonts/knk.woff");
+		
+		
+		BorderPane mainWindow = new BorderPane();
+		try {
+			mainWindow.setTop(Header.display("Jetmir Avdullahu", "Super Admin", current));	
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Scene scene = new Scene(mainWindow, 800, 100);
+		primaryStage.setScene(scene);
+		primaryStage.setMaximized(true);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+
+		launch(args);
+	}
+
+}
