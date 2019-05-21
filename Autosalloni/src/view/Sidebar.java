6775a5c8b315a45926ad1af08c5d8ff692edeccd
Hajrije <44509293+Hajrije@users.noreply.github.com>
2019-05-21 13:24:44 +0200

    setAlignment to icons

--- Autosalloni/src/view/Sidebar.java
+++ Autosalloni/src/view/Sidebar.java
@@ -46,6 +46,7 @@ public class Sidebar {
 		dashboardBtn.setGraphic(dashboardView);
 		dashboardBtn.setPrefWidth(200);
 		dashboardBtn.setPrefHeight(50);
+		dashboardBtn.setAlignment(Pos.CENTER_LEFT);
 		dashboardBtn.getStyleClass().add("sideBarBtn");
 		
 		
@@ -59,6 +60,7 @@ public class Sidebar {
 		storesBtn.setGraphic(storesView);
 		storesBtn.setPrefWidth(200);
 		storesBtn.setPrefHeight(50);
+		storesBtn.setAlignment(Pos.CENTER_LEFT);
 		storesBtn.getStyleClass().add("sideBarBtn");
 		
 		Button carsBtn = new Button("Cars");
@@ -67,6 +69,7 @@ public class Sidebar {
 		carsBtn.setGraphic(carView);
 		carsBtn.setPrefWidth(200);
 		carsBtn.setPrefHeight(50);
+		carsBtn.setAlignment(Pos.CENTER_LEFT);
 		carsBtn.getStyleClass().add("sideBarBtn");
 		
 		
@@ -79,6 +82,7 @@ public class Sidebar {
 		usersBtn.setGraphic(usersView);
 		usersBtn.setPrefWidth(200);
 		usersBtn.setPrefHeight(50);
+		usersBtn.setAlignment(Pos.CENTER_LEFT);
 		usersBtn.getStyleClass().add("sideBarBtn");
 		
 		Button manufacturersBtn = new Button("Manufacturer");
@@ -97,6 +101,7 @@ public class Sidebar {
 		reportsBtn.setGraphic(reportsView);
 		reportsBtn.setPrefWidth(200);
 		reportsBtn.setPrefHeight(50);
+		reportsBtn.setAlignment(Pos.CENTER_LEFT);
 		reportsBtn.getStyleClass().add("sideBarBtn");
 		
 		Button settingsBtn = new Button("Settings");
@@ -105,6 +110,7 @@ public class Sidebar {
 		settingsBtn.setGraphic(settingsView);
 		settingsBtn.setPrefWidth(200);
 		settingsBtn.setPrefHeight(50);
+		settingsBtn.setAlignment(Pos.CENTER_LEFT);
 		settingsBtn.getStyleClass().add("sideBarBtn");
 		
 		
